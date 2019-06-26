2a496fb8f8c62ea260087ecb40cd045806a25827
Elliott Chenoweth <elliott.chenoweth@gmail.com>
2019-06-26 17:06:47 -0400

    Add Isogram.cs solution.

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+		if (String.IsNullOrEmpty(word))
+			return true;
+
+		word = word.ToLower();
+
+		HashSet<char> letter_count = new HashSet<char>();
+
+		foreach (char c in word)
+		{
+			if (Char.IsLetter(c))
+			{
+				if (letter_count.Contains(c))
+					// duplicate letter
+					return false;
+				else
+					letter_count.Add(c);
+			}
+		}
+		return true;
+			
+    }
+}
