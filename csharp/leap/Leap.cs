8480f6198f2752f687282bb3e271cc38f7a30fcf
Elliott <elliott.chenoweth@gmail.com>
2019-06-27 06:28:37 -0400

    Leap.cs v3

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,15 +2,6 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-		if (year % 4 == 0)
-		{
-			if (year % 400 == 0)
-				return true;
-			else if (year % 100 == 0)
-				return false;
-			else
-				return true;
-		}
-		return false;
+	    return year % 4 == 0 && (year % 100 != 0) || (year % 400 == 0);
     }
 }
