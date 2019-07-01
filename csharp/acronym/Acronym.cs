50d36241ef524b0aac31f7881e57f088ea413023
Elliott Chenoweth <elliott.chenoweth@gmail.com>
2019-07-01 13:21:28 -0400

    Add Acronym.cs solution.

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    private static char[] delimiterChars = { ' ', '-' };
+    public static string Abbreviate(string phrase)
+    {
+        var words = phrase
+            .Replace('_', ' ')
+            .Split(Acronym.delimiterChars, 
+                   StringSplitOptions.RemoveEmptyEntries);
+        
+        string firstLetters = new string(
+            words
+                .Select(s => s[0])
+                .ToArray());
+
+        return firstLetters.ToUpper();
+    }
+}
